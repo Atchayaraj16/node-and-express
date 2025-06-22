1fd5da245afd1df29da22206ed58cd8856950370
Atchayaraj16 <atchayaraj16@gmail.com>
2025-06-23 00:37:11 +0530

    adding controllers for perfect routing

---
+++ controllers/movie_contoller.js
@@ -0,0 +1,16 @@
+export const movieindex=(req,res)=>
+{
+    res.send('get all movies');
+};
+export const moviecreate=(req,res)=>
+{
+    res.send('create all movies');
+};
+export const movieupdate =(req,res)=>
+{
+     res.send('update all movies');
+};
+export const moviedelete=(req,res)=>
+{
+   res.send('delete all movies');
+};
--- routers/movie_route.js
+++ routers/movie_route.js
@@ -1,26 +1,22 @@
 import express from 'express';
+import{
+    movieindex,
+    moviecreate,
+    movieupdate,
+    moviedelete,
+    
+
+}from '../conrollers/movie_controller.js';
  
 const router =express.Router();
 // crud functionality of movies
 // for reading
-router.get('/',(req,res)=>
-{
-    res.send('get all movies');
-})
-// for creation
-router.post('/',(req,res)=>
-{
-    res.send('create all movies');
-})
-//for update
-router.put('/:id',(req,res)=>
-{
-     res.send('update all movies');
-})
+router.get('/',movieindex);
 
+// for creation
+router.post('/',oviecreate);
+//for update
+router.put('/:id',movieupdate);
 //for delete
-router.delete('/:id',(req,res)=>
-{
-   res.send('delete all movies');
-})
+router.delete('/:id',moviedelete);
 export default router;
